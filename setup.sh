0139f2e852e4fcc627732ca7c8b15c616538d8c0
LujainAb <Lujainmeab@gmail.com>
2021-09-07 18:07:26 +0300

    setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+export DATABASE_URL="postgres://hurozjgnthtarz:3cb0d161fd03f35308dfb7c94a2abbff5ba6b11c3e3fc8f45da0f82a3d4ebeaa@ec2-44-195-201-3.compute-1.amazonaws.com:5432/dc00vqkdatqltl"
+export AUTH0_DOMAIN='lujainexp.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='Mascot'
+
+
+export client_id='yWHYBoHSfZ8OAoCnxg2Xo59YxX5PkcIB',
+export client_secret='4_EwlUlAWGWkU808zVnIamxmB_dhQTKELeRNc2Apjxgkyvm_d_NTMKJJu8EiGnDc',
+# export api_base_url='https://lujainexp.us.auth0.com',
+# export access_token_url='https://lujainexp.us.auth0.com/oauth/token',
+# export authorize_url='https://lujainexp.us.auth0.com/authorize',
+
+
+# Tokens
+
