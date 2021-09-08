fdbe280d6b95bc9400225f6ee81a8b2d66fc2d9a
LujainAb <Lujainmeab@gmail.com>
2021-09-08 18:10:03 +0300

    added hosted url

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ export ALGORITHMS=['RS256']
 export API_AUDIENCE='Mascot'
 export client_id='yWHYBoHSfZ8OAoCnxg2Xo59YxX5PkcIB'
 export client_secret='4_EwlUlAWGWkU808zVnIamxmB_dhQTKELeRNc2Apjxgkyvm_d_NTMKJJu8EiGnDc'
-# export api_base_url='https://lujainexp.us.auth0.com',
-# export access_token_url='https://lujainexp.us.auth0.com/oauth/token',
-# export authorize_url='https://lujainexp.us.auth0.com/authorize',
+
+
+# Login url https://lujainexp.us.auth0.com/authorize?audience=Mascot&response_type=token&client_id=yWHYBoHSfZ8OAoCnxg2Xo59YxX5PkcIB&redirect_uri=https://mascotashop.herokuapp.com/
 
 
 # Tokens
